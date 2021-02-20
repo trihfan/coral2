b4f9a1f7ce1b19e643a3f8fe50efdeccb21fe342
Thibault Cardaire <thibault.cardaire@gmail.com>
2021-02-20 13:34:50 +0100

    sync

--- tests/web/main.cpp
+++ tests/web/main.cpp
@@ -12,13 +12,49 @@
 #include <chrono>
 #include <fstream>
 #include <gl/glew.h>
+#include <iostream>
 #include <numeric>
 #include <thread>
 #include <vector>
 
 #include <GLFW/glfw3.h>
+//#include <websocketpp/config/asio_no_tls.hpp>
+//#include <websocketpp/server.hpp>
 
 using namespace coral;
+
+/*typedef websocketpp::server<websocketpp::config::asio> server;
+using websocketpp::lib::bind;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;*/
+
+// pull out the type of messages sent by our config
+/*typedef server::message_ptr message_ptr;
+
+// Define a callback to handle incoming messages
+void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg)
+{
+    Logs(info) << "on_message called with hdl: " << hdl.lock().get()
+               << " and message: " << msg->get_payload();
+
+    // check for a special command to instruct the server to stop listening so
+    // it can be cleanly exited.
+    if (msg->get_payload() == "stop-listening")
+    {
+        s->stop_listening();
+        return;
+    }
+
+    try
+    {
+        s->send(hdl, msg->get_payload(), msg->get_opcode());
+    }
+    catch (websocketpp::exception const& e)
+    {
+        Logs(error) << "Echo failed because: "
+                    << "(" << e.what() << ")";
+    }
+}*/
 
 // screen
 int width = 800;
@@ -48,10 +84,42 @@ int main(int argc, char** argv)
     }
 
     // encoder
-    if (!initializeEncoder())
+    /* if (!initializeEncoder())
     {
         return 1;
+    }*/
+
+    // Create a server endpoint
+    /* server echo_server;
+    try
+    {
+        // Set logging settings
+        echo_server.set_access_channels(websocketpp::log::alevel::all);
+        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+        // Initialize Asio
+        echo_server.init_asio();
+
+        // Register our message handler
+        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
+
+        // Listen on port 9002
+        echo_server.listen(9002);
+
+        // Start the server accept loop
+        echo_server.start_accept();
+
+        // Start the ASIO io_service run loop
+        echo_server.run();
     }
+    catch (websocketpp::exception const& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "other exception" << std::endl;
+    }*/
 
     // Main loop
     std::ofstream output("output.h264");
