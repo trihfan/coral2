cacb476b2be07098d9416d239cbb2e5db801ab7e
thibault cardaire <tcardaire@sophiagenetics.com>
2021-02-22 20:09:15 +0100

    sync

--- backends/opengl/OpenglPipeline.cpp
+++ backends/opengl/OpenglPipeline.cpp
@@ -20,36 +20,45 @@ void OpenglPipeline::use()
 
 void OpenglPipeline::setUniform(const std::string& name, bool value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, int value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, float value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::vec2& value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::vec3& value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::vec4& value) const
 {
+    shader->setUniform(name, value);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::mat2& mat) const
 {
+    shader->setUniform(name, mat);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::mat3& mat) const
 {
+    shader->setUniform(name, mat);
 }
 
 void OpenglPipeline::setUniform(const std::string& name, const glm::mat4& mat) const
 {
+    shader->setUniform(name, mat);
 }
--- backends/opengl/Shader.h
+++ backends/opengl/Shader.h
@@ -33,15 +33,15 @@ namespace coral
 
         // utility uniform functions
         // ------------------------------------------------------------------------
-        void setBool(const std::string& name, bool value) const;
-        void setInt(const std::string& name, int value) const;
-        void setFloat(const std::string& name, float value) const;
-        void setVec2(const std::string& name, const glm::vec2& value) const;
-        void setVec3(const std::string& name, const glm::vec3& value) const;
-        void setVec4(const std::string& name, const glm::vec4& value) const;
-        void setMat2(const std::string& name, const glm::mat2& mat) const;
-        void setMat3(const std::string& name, const glm::mat3& mat) const;
-        void setMat4(const std::string& name, const glm::mat4& mat) const;
+        void setUniform(const std::string& name, bool value) const;
+        void setUniform(const std::string& name, int value) const;
+        void setUniform(const std::string& name, float value) const;
+        void setUniform(const std::string& name, const glm::vec2& value) const;
+        void setUniform(const std::string& name, const glm::vec3& value) const;
+        void setUniform(const std::string& name, const glm::vec4& value) const;
+        void setUniform(const std::string& name, const glm::mat2& mat) const;
+        void setUniform(const std::string& name, const glm::mat3& mat) const;
+        void setUniform(const std::string& name, const glm::mat4& mat) const;
 
     private:
         std::array<std::string, 3> shader_data;
